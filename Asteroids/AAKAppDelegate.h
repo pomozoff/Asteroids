f6ebf6c283df5105ab3b029664aa6e5199ed44da
Anton Pomozov <pomozov@shtrih-m.ru>
2014-06-26 15:53:49 +0400

    Initial Commit

---
+++ Asteroids/AAKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AAKAppDelegate.h
+//  Asteroids
+//
+//  Created by Anton Pomozov on 26.06.14.
+//  Copyright (c) 2014 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Asteroids/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Asteroids
+//
+//  Created by Anton Pomozov on 26.06.14.
+//  Copyright (c) 2014 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AAKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AAKAppDelegate class]));
+    }
+}
